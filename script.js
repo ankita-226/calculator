3251443d498cdb504343bb02e28441e13ecd226d
Ankita Yadav <ankitayaduvanshi226@gmail.com>
2023-04-01 18:37:47 +0530

    function modified

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let input =document.getElementById("input-box")
 let button=document.querySelectorAll("button")
 
 let string =""
-let arr=Array.from(button)
-arr.forEach(button=>{
+// let arr=Array.from(button)
+button.forEach(button=>{
     button.addEventListener("click",(e)=>{
         if(e.target.innerHTML== "="){
-            string=eval(string)
-            input.value=string
+            calculateNumber=eval(string)
+            input.value=calculateNumber
         }
         else if(e.target.innerHTML=='AC')
         {
@@ -15,7 +15,7 @@ arr.forEach(button=>{
             input.value=string
         }
         else if(e.target.innerHTML=='DEL'){
-            string=string.substring(0,  str.length-1)
+            string=string.substring(0,  string.length-1)
             input.value=string
         }
         else{
